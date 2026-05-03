ab46cdda49eb94d34f52a90dfbc23a0facc5a589
Caio Henrique <caiohamoreira@gmail.com>
2026-05-03 13:53:32 -0300

    First Commit

---
+++ courseUdemySpringboot/src/main/java/com/devSpringboot/courseUdemySpringboot/CourseUdemySpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.devSpringboot.courseUdemySpringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseUdemySpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseUdemySpringbootApplication.class, args);
+	}
+
+}
---
+++ courseUdemySpringboot/src/test/java/com/devSpringboot/courseUdemySpringboot/CourseUdemySpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devSpringboot.courseUdemySpringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseUdemySpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
