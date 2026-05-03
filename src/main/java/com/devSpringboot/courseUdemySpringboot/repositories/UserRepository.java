c419a974019d11a910f6950ffb65cdc6f89f467e
Caio Henrique <caiohamoreira@gmail.com>
2026-05-03 18:04:56 -0300

    add:  Camada de serviço, novo EndPoint(FindById)

--- src/main/java/com/devSpringboot/courseUdemySpringboot/repositories/UserRepository.java
+++ src/main/java/com/devSpringboot/courseUdemySpringboot/repositories/UserRepository.java
@@ -2,6 +2,8 @@ package com.devSpringboot.courseUdemySpringboot.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.devSpringboot.courseUdemySpringboot.entities.User;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
---
+++ src/main/java/com/devSpringboot/courseUdemySpringboot/services/UserService.java
@@ -0,0 +1,25 @@
+package com.devSpringboot.courseUdemySpringboot.services;
+
+import com.devSpringboot.courseUdemySpringboot.entities.User;
+import com.devSpringboot.courseUdemySpringboot.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
